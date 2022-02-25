ca53ec85fbde6e79e36e1be3f82ee9e41ec394ee
Chidinma02 <nancee801@gmail.com>
2022-02-25 14:42:40 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="app" :class="mode">
+    <HelloWorld :mode="mode"></HelloWorld>
+
+    <router-view />
+    <FooTer></FooTer>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import FooTer from "./components/FooTer.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    FooTer,
+  },
+
+  data() {
+    return {
+      // mode: "light",
+    };
+  },
+};
 </script>
 
 <style>
-#app {
+/*.app.navi {
+  background-color: white !important;
+  color: black !important;
+}
+.dark.navi{
+  background-color: black !important;
+  color: white !important;
+}*/
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}*/
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="nv">
+      <b-navbar toggleable="lg" class="nv pt-3 container navbar-dark">
+        <b-navbar-brand href="#" class="navi">CO.</b-navbar-brand>
+
+        <b-navbar-toggle class="tar" target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <!-- <b-nav-item href="#">Link</b-nav-item> -->
+            <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#" class="ml-5"
+              ><router-link to="/" class="navi">Home</router-link></b-nav-item
+            >
+            <b-nav-item href="#" class="ml-5"
+              ><router-link to="/about" class="navi"
+                >About</router-link
+              ></b-nav-item
+            >
+            <b-nav-item
+              href="https://chidinmablog.hashnode.dev/"
+              class="ml-5 navi"
+            >
+              Blog
+            </b-nav-item>
+            <b-nav-item
+              href="https://github.com/Chidinma02/OkwuohaChidinma.git"
+              class="ml-5 navi"
+            >
+              Github
+            </b-nav-item>
+
+            <a
+              href="../assets/Chidinma's Resume (1).pdf"
+              download
+              class="btn btn-lg butt pl-3 pr-3 ml-5 navi"
+              >Download Cv</a
+            >
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  // props: {
+  // msg: String,
+  // },
+  props: ["mode"],
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+* {
+  color: white !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.nv {
+  background-color: black;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.butt {
+  background-color: black;
+  color: white;
+  border: 2px solid;
+  border: 1px solid #15bf50;
 }
-a {
-  color: #42b983;
+.navi {
+  color: white !important;
+  font-weight: bold;
+}
+.tar {
+  background-color: black;
+  color: white !important;
+}
+.b-nav-item {
+  color: white !important;
 }
 </style>
