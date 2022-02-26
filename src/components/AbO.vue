01f805560e69444dcd37b4c015357b9aa2453d18
Chidinma02 <nancee801@gmail.com>
2022-02-26 08:01:24 +0100

    'com'

--- src/components/AbO.vue
+++ src/components/AbO.vue
@@ -5,17 +5,18 @@
       <div class="row">
         <div class="col-12">
           <p class="de">
-            Hello!👋🏾 My name is Okwuoha Chidinma,I am a frontend developer and
-            technical writer. My early childhood was spent in a very interesting
-            environment and among intelligent friends leading me to develop an
-            open minded personality , and a clear state of mind. Growing up I
-            found myself falling in love with technology, thinking futuristic
-            and trying to learn new things related to software development. As a
-            frontend developer, I develop and maintain the User interface,
-            consuming APIs, while also staying through to the design and
-            bringing it to life,using Html,Css,JavaScript,Jquery, Vue.js,Nuxt
-            and bootstrap. I'm confident, naturally curious, and perpetually
-            working on improving my skills.
+            Hello!👋🏾 My name is <span class="ok">Chidinma Okwuoha</span> ,I am a
+            frontend developer and technical writer. My early childhood was
+            spent in a very interesting environment and among intelligent
+            friends leading me to develop an open minded personality , and a
+            clear state of mind. Growing up I found myself falling in love with
+            technology, thinking futuristic and trying to learn new things
+            related to software development. As a frontend developer, I develop
+            and maintain the User interface, consuming APIs, while also staying
+            through to the design and bringing it to life,using
+            Html,Css,JavaScript,Jquery, Vue.js,Nuxt and bootstrap. I'm
+            confident, naturally curious, and perpetually working on improving
+            my skills.
           </p>
         </div>
       </div>
@@ -49,5 +50,8 @@ export default {
 .fon {
   font-family: "Petrona", serif;
   font-weight: bolder;
+}
+.ok {
+  color: #15bf50;
 }
 </style>
